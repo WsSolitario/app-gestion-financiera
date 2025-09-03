959e2d9220f2b52ad6f0d110e245c895dbfba8a8
WsSolitario <108768148+WsSolitario@users.noreply.github.com>
2025-09-02 22:07:18 -0600

    feat: implement API service methods

--- lib/services/group_service.dart
+++ lib/services/group_service.dart
@@ -1,6 +1,54 @@
+import "package:dio/dio.dart";
+
+import "../models/group.dart";
 import "../services/api_client.dart";
 
 class GroupService {
   final ApiClient _client;
   GroupService(this._client);
+
+  Future<List<Group>> getGroups() async {
+    try {
+      final res = await _client.get("/groups");
+      final data = res.data as List;
+      return data.map((e) => Group.fromJson(e)).toList();
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
+
+  Future<Group> getGroup(String id) async {
+    try {
+      final res = await _client.get("/groups/$id");
+      return Group.fromJson(res.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
+
+  Future<Group> createGroup(String name) async {
+    try {
+      final res = await _client.post("/groups", data: {"name": name});
+      return Group.fromJson(res.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
+
+  Future<Group> updateGroup(String id, {required String name}) async {
+    try {
+      final res = await _client.put("/groups/$id", data: {"name": name});
+      return Group.fromJson(res.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
+
+  Future<void> deleteGroup(String id) async {
+    try {
+      await _client.delete("/groups/$id");
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
 }
--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,6 +1,33 @@
+import "package:dio/dio.dart";
+
 import "../services/api_client.dart";
 
 class NotificationService {
   final ApiClient _client;
   NotificationService(this._client);
+
+  Future<List<dynamic>> getNotifications() async {
+    try {
+      final res = await _client.get("/notifications");
+      return List<dynamic>.from(res.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
+
+  Future<void> markAsRead(String id) async {
+    try {
+      await _client.post("/notifications/$id/read");
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
+
+  Future<void> deleteNotification(String id) async {
+    try {
+      await _client.delete("/notifications/$id");
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    }
+  }
 }
